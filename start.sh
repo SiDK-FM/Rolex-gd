3c8867ca3665ef08517d6a124b1335a70c9a57ec
Vikram Singh <122893366+VigaRepo@users.noreply.github.com>
2023-04-12 10:10:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-pip install --trusted-host pypi.python.org -r requirements.txt
+pip3 install --trusted-host pypi.python.org -r requirements.txt
 
 # Start the WebStreamer
 python3 -m WebStreamer
