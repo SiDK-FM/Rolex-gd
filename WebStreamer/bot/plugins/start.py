0e36fba87944bcaa4fe138adae3fbbb63d1c3caf
SiDK <127277028+SiDK-FM@users.noreply.github.com>
2023-05-28 13:58:32 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -10,13 +10,11 @@ from WebStreamer.utils import get_hash, get_name
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-@StreamBot.on_message(filters.command(["start", "help"]))
+@StreamBot.on_message(filters.command(["start", "Rolexhelp"])) 
 async def start(_, m: Message):
-    message = f"Hello {m.from_user.mention(style='md')},\n\n"
-    message += "Send me a file or You can just add me any Telegram Channel and Use.\n"
-    message += "<b>Example:-</b>\n"
-    message += "<i>https://file2link.vigabots.rf.gd/1001901987607/1</i>\n\n"
-    message += "<i>https://file2link.vigabots.rf.gd/Your_Channel_ID/Message_ID</i>\n\n"
-    message += "<b>Join My Updates Channel :- @Link_Leech</b>\n"
-    message += "<b>Join My Support Group :- @Viga_Bots_Discussion</b>"
+    message = f"ʜᴇʟʟᴏ {m.from_user.mention(style='md')},\n\n"
+    message += "sᴇɴᴅ ᴍᴇ ᴀ ғɪʟᴇ ᴏʀ ʏᴏᴜ ᴄᴀɴ ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴀɴʏ ᴛᴇʟᴇɢʀᴀᴍ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴜsᴇ.\n"
+    message += "<b>ᴊᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ :- @SkymoviesHDX</b>\n"
     await m.reply(message)
+    
+
